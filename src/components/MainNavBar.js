0d80d4e48f1dabb821735a09b31b46b36a33d15e
ronythankachan <rony.thankachan@cognicor.com>
2020-09-04 13:57:19 +0530

    navbar collapse on click

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -3,23 +3,29 @@ import {Navbar, Nav} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 import '../App.css';
-function MainNavBar(){
+class MainNavBar extends React.Component{
+
+  state={
+    navSelected:false
+  }
+
+  collpaseNav(){
+    this.setState({navSelected:!this.state.navSelected});
+  }
+  render(){
     return(
-        <Navbar className="nav-font" sticky="top" bg="white" variant="light" expand="lg">
+      <Navbar className="nav-font" sticky="top" bg="white" variant="light" expand="lg" onClick={()=>{this.blah()}} expanded={this.state.navSelected}>
         <Navbar.Brand className="brand-font" href="/">draner.ai</Navbar.Brand>
         <Navbar.Toggle className="border-0" aria-controls ="navbar-toggle" />
         <Navbar.Collapse id="navbar-toggle">
-          <Nav className="m-auto margin-right">
-            {/* <Link className="nav-link" to="/about">About</Link> */}
+          <Nav className="m-auto margin-right"  onClick={()=>{this.collpaseNav();}}>
             <Link className="nav-link" to="/projects">Projects</Link>
             <Link className="nav-link" to="/blog">Blog</Link>
             <Link className="nav-link" to="/contact">Contact</Link>
           </Nav>
-          {/* <Form inline>
-            <FormControl type="text" placeholder="Search" className=" mr-sm-2" style={{borderRadius:'.7rem'}} />
-          </Form> */}
         </Navbar.Collapse>
-      </Navbar>
+    </Navbar>
     );
+  }
 }
 export default MainNavBar;
